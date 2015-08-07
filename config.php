b1339ad9198e761d81835e64c121f71bfd3fce3d
coffeetalkh <mehdi.hosseini.dev@gmail.com>
2015-08-08 00:28:58 +0430

    Add lang files and improve config file

--- config.php
+++ config.php
@@ -5,11 +5,62 @@ return array(
   'timezone' => 'Asia/Tehran',
 
                 /**
+                 *
                  * Here is some other area
-                'Asia/Kabul,
 
-                'Europe/london'
+                 'Asia/Kabul,
+
+                 'Europe/london',
+
                 */
+
+  'evets' =>      'Iran' => array( '1' => 'nowruz', 2 => 'nowruz', 3 => 'nowruz', 4 => 'nowruz', 12 => 'Islamic Republic of Iran national day', 13 => '13 bedar'),
+
+                  /**
+                   * Array is between 1 and 365 day of year
+
+                   'Afghanistan',
+
+                   'Tajikestan',
+
+                   'General',
+
+                   */
+
+
+  'language' =>     'Persian',
+
+                    /**
+                     *
+
+                    'English',
+
+                    */
+
+  'calendar' =>      'Iran',
+
+                     /**
+                      *
+
+                      'Afghanistan',
+
+                      'Jalali',
+
+                      'Gregorian',
+
+                      */
+
+  'weekend' =>        'Iran'  => array( 'friday' ),
+
+                       /**
+                        *
+
+                        'General'  => array( 'saturday', 'sunday' ),
+
+                        'Manualy' => array(),
+
+                        */
+
 
  );
 
---
+++ lang/fa/general.php
@@ -0,0 +1,14 @@
+<?php
+
+  return array(
+
+         'nowruz' => 'عید نوروز',
+
+         'Islamic Republic of Iran national day' => 'روز جمهوری اسلامی',
+
+         '13 bedar' => '۱۳ بدر',
+         
+
+  );
+
+?>
