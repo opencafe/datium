95bc3b97df3d41cec513b68678305570e21321ef
mehdi hosseini <mehdi.hosseini.dev@gmail.com>
2016-02-25 02:00:56 +0330

    Update readme

---
+++ src/lang/en/general.php
@@ -0,0 +1,41 @@
+<?php
+
+  return [
+        '0' => '0',
+        '1' => '1',
+        '2' => '2',
+        '3' => '3',
+        '4' => '4',
+        '5' => '5',
+        '6' => '6',
+        '7' => '7',
+        '8' => '8',
+        '9' => '9',
+        'January' => 'January',
+        'February' => 'February',
+        'March' => 'March',
+        'April' => 'April',
+        'May' => 'May',
+        'June' => 'June',
+        'July' => 'July',
+        'August' => 'August',
+        'September' => 'September',
+        'October' => 'October',
+        'November' => 'November',
+        'December' => 'December',
+        'Monday' => 'Monday',
+        'Tuesday' => 'Tuesday',
+        'Wednesday' => 'Wednesday',
+        'Thursday' => 'Thursday',
+        'Friday' => 'Friday',
+        'Saturday' => 'Saturday',
+        'Sunday' => 'Sunday',
+        'th' => 'th',
+        'AM' => 'AM',
+        'PM' => 'PM',
+        'Jumada I' => 'Jumada I',
+        'Shanbe' => 'Shanbe',
+        'Tir' => 'Tir'
+  ];
+
+?>
---
+++ tests/ConvertTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Datium\Datium;
+
+class ConvertTest extends PHPUnit_Framework_TestCase {
+
+  public function testGregorianToJalali() {
+
+    $date = Datium::create( 2016, 1, 25 )->to( 'jalali' )->get();
+
+    $this->assertEquals( '1394-11-05 00:00:00', $date );
+
+  }
+
+  public function testGregorianToHijri() {
+
+    $date = Datium::create( 2016, 1, 25 )->to( 'hijri' )->get();
+
+    // $this->assertEquals( '1437-04-15 00:00:00', $date );
+
+  }
+
+  public function testJalaliToGregorian() {
+
+    $date = Datium::create( 1394, 11, 5 )->from('jalali')->get();
+
+    // $this->assertEquals( '2016-01-25 00:00:00', $date );
+
+  }
+
+  public function testHijriToGregorian() {
+
+    $date = Datium::create( 1437, 4, 15 )->from('hijri')->get();
+
+    // $this->assertEquals( '2016-01-25 00:00:00', $date );
+
+  }
+
+  public function testJalaliToHijri() {
+
+    $date = Datium::create( 1394, 11, 5 )->from('jalali')->to('hijri')->get();
+
+    // $this->assertEquals( '1437-04-15 00:00:00', $date );
+
+  }
+
+  public function testHijriToJalali() {
+
+    $date = Datium::create( 1437, 04, 15 )->from('hijri')->to('jalali')->get();
+
+    // $this->assertEquals( '1394-11-05 00:00:00', $date );
+
+  }
+
+}
