557cb7f5b4d25efe87ba995ce1dd34cf5339a6fa
ShahrokhNiakan <shahrokhniakan15@gmail.com>
2017-07-29 20:32:58 +0430

    Add kurdish calendar

--- tests/DayOfTest.php
+++ tests/DayOfTest.php
@@ -18,6 +18,12 @@ class DayOfTest extends \PHPUnit_Framework_TestCase
         // 1393-10-11 - 287th day
         $this->assertEquals(287, Datium::create(2015, 1, 1)->to('jalali')->dayOf()->year());
 
+        // 2014-12-19 - 353th day
+        $this->assertEquals(353, Datium::create(2015, 1, 1)->to('julian')->dayOf()->year());
+
+        // 2714-10-11 - 287th day
+        $this->assertEquals(287, Datium::create(2015, 1, 1)->to('kurdish')->dayOf()->year());
+
     }
 
     public function testDayOfWeek()
@@ -30,18 +36,22 @@ class DayOfTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(6, Datium::create(2015, 1, 1)->to('jalali')->dayOf()->week());
 
+        $this->assertEquals(6, Datium::create(2015, 1, 1)->to('kurdish')->dayOf()->week());
+
     }
 
 	public function testLastDayOfMonth()
 	{
-		// leap year
-		$this->assertEquals(29, Datium::create(2016, 2, 1)->dayOf()->lastDayMonth());
-		
-		$this->assertEquals(31, Datium::create(2015, 1, 1)->dayOf()->lastDayMonth());
+	    // leap year
+        $this->assertEquals(29, Datium::create(2016, 2, 1)->dayOf()->lastDayMonth());
+
+        $this->assertEquals(31, Datium::create(2015, 1, 1)->dayOf()->lastDayMonth());
 
         $this->assertEquals(30, Datium::create(2015, 1, 1)->to('hijri')->dayOf()->lastDayMonth());
 
         $this->assertEquals(30, Datium::create(2015, 1, 1)->to('jalali')->dayOf()->lastDayMonth());
+
+        $this->assertEquals(30, Datium::create(2015, 1, 1)->to('kurdish')->dayOf()->lastDayMonth());
 
 	}
 }
